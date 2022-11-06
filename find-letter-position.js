1de6bb4214d531a43c8a4418be524163a41d67f4
robmill7 <83546965+robmill7@users.noreply.github.com>
2022-11-06 09:11:17 -0500

    Create find-letter-position.js

---
+++ find-letter-position.js
@@ -0,0 +1,6 @@
+// takes a letter input and returns its position in alphabet
+
+function position(letter){
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  return 'Position of alphabet: ' + (alphabet.indexOf(letter) + 1);
+}
