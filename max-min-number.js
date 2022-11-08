1e6e84dd1df49971814cc625a2b2bce9f47ddc96
robmill7 <83546965+robmill7@users.noreply.github.com>
2022-11-08 09:02:35 -0500

    Create max-min-number.js

---
+++ max-min-number.js
@@ -0,0 +1,9 @@
+var min = function(list){
+    
+    return Math.min(...list) //returns lowest number in list 
+}
+
+var max = function(list){
+    
+    return Math.max(...list) //returns highest number in list 
+}
