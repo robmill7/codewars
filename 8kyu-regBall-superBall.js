3af80331f96421ca6791b4bd25f64c33c876576f
robmill7 <83546965+robmill7@users.noreply.github.com>
2022-12-19 18:02:05 -0500

    Create 8kyu-regBall-superBall.js

---
+++ 8kyu-regBall-superBall.js
@@ -0,0 +1,23 @@
+/*
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/
+
+
+var Ball = function(ballType) {
+ // if ballType is empty return regular for ballType
+  if(!ballType){
+    ballType = 'regular'
+  }
+  
+    this.ballType = ballType;
+  
+};
+  
