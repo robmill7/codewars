a41d5810727d4420290cc09c22a0956ed5cfb02b
robmill7 <83546965+robmill7@users.noreply.github.com>
2023-01-18 09:11:51 -0500

    Create 8kyu-remove-'!'.js

---
+++ 8kyu-remove-'!'.js
@@ -0,0 +1,11 @@
+/*
+Remove all exclamation marks from the end of sentence.
+
+*/
+
+
+function remove (string) {  
+  // removing exclamation marks from end of sentence
+  return string.replace(/!+$/, '');
+
+}
