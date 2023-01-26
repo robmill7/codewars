a3af83054cf46fcb818a2e81ec1052dbf536ec4e
robmill7 <83546965+robmill7@users.noreply.github.com>
2023-01-26 11:01:49 -0500

    Create 8kyu-shark-pontoon.js

---
+++ 8kyu-shark-pontoon.js
@@ -0,0 +1,39 @@
+/*********************************************************************************************
+You need to work out if the shark will get to you before you can get to the pontoon.
+To make it easier... as you do the mental calculations in the water you either freeze 
+when you realise you are dead, or swim when you realise you can make it!
+
+You are given 5 variables;
+
+sharkDistance = distance from the shark to the pontoon. The shark will eat you if it
+reaches you before you escape to the pontoon.
+
+sharkSpeed = how fast it can move in metres/second.
+
+pontoonDistance = how far you need to swim to safety in metres.
+
+youSpeed = how fast you can swim in metres/second.
+
+dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
+
+The pontoon, you, and the shark are all aligned in one dimension.
+
+If you make it, return "Alive!", if not, return "Shark Bait!".
+
+*******************************************************************************************/
+
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+  let yourTime = pontoonDistance / youSpeed;
+  let sharkTime = sharkDistance / sharkSpeed;
+  
+  if(dolphin){
+    sharkTime = sharkDistance / (sharkSpeed / 2);
+  }
+  if(yourTime < sharkTime){
+    return 'Alive!';
+  }
+  else{
+    return 'Shark Bait!'
+  }
+}
